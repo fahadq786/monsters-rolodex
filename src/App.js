657c08e139596062b0f051b3ce56a74c6a06ee09
EC2 Default User <ec2-user@ip-172-31-10-8.us-west-2.compute.internal>
2020-10-02 02:26:42 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    
+    super();
+    
+    this.state = {
+      string: 'Fahad',
+      num: 1
+    };
+    
+  }
+  
+  render() {
+    
+    return(
+      
+          <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Hello my name is {this.state.string} + {this.state.num}</p>
+              <button onClick={ 
+                  ()=> { 
+                    var newName = '';
+
+                    if (this.state.string === 'Qureshi') {
+                      newName = 'Fahad'
+                    }
+                    else {
+                      newName = 'Qureshi'
+                    }
+                    
+                    this.setState( {
+                      string: newName,
+                      num: this.state.num + 1
+                    })
+
+                    
+                  }
+                  }>
+                    Change Text
+                </button>
+          </header>
+        </div>
+      
+      
+      );
+    
+    
+  }
+  
 }
 
 export default App;
